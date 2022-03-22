8ac8751c35b92a9730eed806e7a437c1b1ba8452
v4mmpp <zebientmax007@gmail.com>
2022-03-22 14:36:29 +0400

    Created manifest file

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+--[[
+    This is a part of disconnectInfos
+    Copyright (©) 2022 - ${RevengeBack_}#4235
+    All rights is reserved.
+]]
+
+fx_version('cerulean') games({ 'gta5' });
+
+server_scripts({
+    
+});
