75a3031039fdb930f5e36ef5824cedf9208186b1
v4mmpp <zebientmax007@gmail.com>
2022-03-22 14:49:23 +0400

    Created main storage menu

--- client/modules/storage_menus.lua
+++ client/modules/storage_menus.lua
@@ -3,3 +3,37 @@
     Copyright (©) 2022 - ${RevengeBack_}#4235
     All rights is reserved.
 ]]
+
+RageUI = (RageUI);
+
+---@public
+---@class vStorageClient_menus
+vStorageClient_menus = {};
+
+---@public
+---@type boolean isStorageMenuOpened
+isStorageMenuOpened = false;
+
+vStorageClient_menus.mainStorageMenu = RageUI.CreateMenu("Coffre", "MENU PRINCIPAL");
+
+---@public
+---@type function openStorageMenu
+function vStorageClient_menus:openStorageMenu()
+    if (isStorageMenuOpened) then
+        isStorageMenuOpened = false;
+        return (self:openStorageMenu());
+    end
+
+    CreateThread(function()
+        isStorageMenuOpened = true;
+        RageUI.Visible(vStorageClient_menus.mainStorageMenu); -- TODO → ADD A MENU
+
+        while (isStorageMenuOpened) do
+
+
+            Wait(2.0)
+        end
+        
+        isStorageMenuOpened = false;
+    end)
+end
